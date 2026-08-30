b408d8de1028ed7bb627d5b91864de6a7e9f6721
Arthur Koba <caprimores@gmail.com>
2026-08-30 12:25:12 +0300

    Adding FH8626 native stream adapter for Divinus

---
+++ tests/fh8626_native_adapter/test_sink_signature.c
@@ -0,0 +1,8 @@
+#include "../../src/hal/full/fh8626_native_adapter.h"
+#include "../../src/media.h"
+
+void fh8626_native_adapter_sink_signature_check(void)
+{
+    fh8626_video_sink sink = save_video_stream;
+    (void)sink;
+}
